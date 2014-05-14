eedb2f5b220d3cdca4fec0de1faffe69999ac9b2
Pedro Souza <predoff@gmail.com>
2014-05-14 16:17:58 -0300

    Changing project info

--- Frapper/Properties/AssemblyInfo.cs
+++ Frapper/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Frapper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Minimalistic ffmpeg wrapper C#")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Pedro Souza")]
 [assembly: AssemblyProduct("Frapper")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
