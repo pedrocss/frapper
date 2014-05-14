0ef72150c334081cd5569637faa2a099d33c2fe9
Pedro Souza <predoff@gmail.com>
2014-05-14 16:44:05 -0300

    Adicionando no nuget

--- Frapper/Properties/AssemblyInfo.cs
+++ Frapper/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Frapper")]
-[assembly: AssemblyDescription("Minimalistic ffmpeg wrapper C#")]
+[assembly: AssemblyDescription("Minimalist ffmpeg wrapper C#")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Pedro Souza")]
 [assembly: AssemblyProduct("Frapper")]
